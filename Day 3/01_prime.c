c2ecb091cfb5a5c33d18116fd454f43d75999539
Ansh Tyagi <anshtyagi6868@gmail.com>
2026-06-05 21:37:16 +0530

    Updated program 01 in Day 3

--- Day 3/01_prime.c
+++ Day 3/01_prime.c
@@ -9,21 +9,24 @@ int main()
     {
         printf("The number is not prime.\n");
     }
-    for(i=2;i<=n/2;i++)
-    {
-        if(n%i==0)
-        {
-            flag=1;
-            break;
-        }
-    }
-    if(flag==0)
-    {
-        printf("The number is prime.\n");
-    }
     else
     {
-        printf("The number is not prime.\n");
-    }   
+        for(i=2;i<=n/2;i++)
+        {
+            if(n%i==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            printf("The number is prime.\n");
+        }
+        else
+        {
+            printf("The number is not prime.\n");
+        }
+    }
     return 0;
 }
